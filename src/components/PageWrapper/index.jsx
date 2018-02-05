6218bc2adf73345e42a65c62988b284eca33edd0
joshb <josh@bravenew.world>
2018-02-05 19:09:21 -0400

    Add page wrapper component for different routes

---
+++ src/components/PageWrapper/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class PageWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+
+  render() {
+    return(
+      //<Header/>
+      <div>
+        {/*MainBody*/}
+      </div>
+
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        HI
+        
       </div>
     );
   }
