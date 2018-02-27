ccf065454b79299f94fe54609255f945018e5005
joshb <josh@bravenew.world>
2018-02-27 17:08:50 -0400

    Add build command for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: [
@@ -17,6 +18,13 @@ module.exports = {
   resolveLoader: {
     root: path.join(__dirname, "node_modules")
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    })
+  ],
   module: {
     loaders: [
       {
