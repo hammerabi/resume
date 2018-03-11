6d1ee293f7f5eb8e339853e3327c981c5658674c
joshb <josh@bravenew.world>
2018-03-11 11:14:15 -0300

    Add skills block component

---
+++ src/scenes/Skills/components/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+require("./index.scss");
+
+export default class SkillsBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+SkillsBlock.PropTypes = {
+  
+};
